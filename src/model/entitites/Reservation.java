30f6ffe48dfba7bed6cc516eae81e4ac018100f1
Allisson Bezerra <allisson.geografia@gmail.com>
2022-10-06 16:56:21 -0300

    versao muito ruim

---
+++ src/model/entitites/Reservation.java
@@ -0,0 +1,69 @@
+package model.entitites;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkout;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+		
+	}
+
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	
+	public Date getCheckout() {
+		return checkout;
+	}
+
+	// jeito padrão de calcular data no java(calculo em milisegundo)
+	public long duration() {
+		long diff =  (checkin.getTime() - checkout.getTime()) *-1;
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkin, Date checkout) {
+		this.checkin = checkin;
+		this.checkout = checkout;
+		
+	}
+
+	@Override
+	public String toString() {
+		// padrão pra formatar a data sair como solicitada // chamanos o metodo no to String tb
+		return "Room"
+				+ roomNumber
+				+ ", Check-in: "
+				+ sdf.format(checkin)
+				+ ", Chek-out: "
+				+ sdf.format(checkout)
+				+ " , "
+				+ duration()
+				+ " nigths"
+				;
+	}
+
+}
